57bed9a3452c3c4bad8a792dca012f4457b7d47a
AugTesting <111387246+AugTesting@users.noreply.github.com>
2022-10-20 17:20:39 -0400

    addedcommit

--- src/test/java/DPages/DAlerts.java
+++ src/test/java/DPages/DAlerts.java
@@ -3,6 +3,7 @@ package DPages;
 import org.openqa.selenium.By;
 
 public class DAlerts {
+	//test comment
 	public static By SimpleAlertBox=By.name("alertbox");
 	public static By ConfirmTextBox=By.name("confirmalertbox");
 	public static By ConfirmTextOkMessage=By.xpath("//p[text()='You clicked on OK!']");
